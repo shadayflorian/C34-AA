9c8d6b44af14c4d2eb56fbb0223740adbe9a70cb
Shaday Florian <77638738+shadayflorian@users.noreply.github.com>
2023-05-24 14:53:05 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,56 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var holder,ball,ground;
+var stand1,stand2;
+var ball;
+var slingShot;
+var fruit;
+function preload(){
+  backgroundImg = loadImage("background.png");
+ fruit=loadImage("melon.png");
+ g=loadImage("basket.png")
+}
+function setup() {
+  createCanvas(900,400);
+  engine = Engine.create();
+  world = engine.world;
+  Engine.run(engine);
+  ground = new Ground();
+  
+ 
+
+  //Challenge1:
+  ball = Bodies.(50,200,20);
+  //añadir el cuerpo al mundo
+ 
+//challenge 2: crear un objeto slingshot
+  
+
+}
+function draw() {
+  background(backgroundImg); 
+ 
+  //Engine.update(engine);
+  //text(mouseX + ',' + mouseY, 10, 15);
+  
+  ground.display();
+  g.scale=.025;
+
+
+  imageMode(CENTER)
+  image(fruit ,ball.position.x,ball.position.y,40,40);
+  image(g,450,270)
+
+  slingShot.display();
+}
+function mouseDragged(){
+  Matter.Body.setPosition();
+}
+function mouseReleased(){
+  slingShot.fly();
+
+}
